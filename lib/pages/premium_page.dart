9bc878aa2c587b621c20c24302612af5cd01cf70
Simu Sohil R <simusohil@gmail.com>
2024-07-08 19:59:38 +0530

    assets for premium page

--- lib/pages/premium_page.dart
+++ lib/pages/premium_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:spotify_clone/constants.dart';
+import 'package:spotify_clone/widgets/footer.dart';
+import 'package:spotify_clone/widgets/header.dart';
 
 main(){
   runApp(const PremiumPage());
@@ -10,11 +13,41 @@ class PremiumPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: true,
       home: SafeArea(
         child: Scaffold(
-          
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(60.0), 
+            child: AppBar(
+              flexibleSpace: const HeaderPage(),
+            )
+          ),
+          body: const PremiumPageView(),
+          bottomNavigationBar: const Footer(),
         )
       ),
+    );
+  }
+}
+
+class PremiumPageView extends StatelessWidget {
+  const PremiumPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: gradient
+        ),
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      )
     );
   }
 }
